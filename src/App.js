a63cccbaf5aef9f692d12c0f0d253c218c7d345d
Tanay Pratap <tanay.mit@gmail.com>
2021-01-27 13:47:59 +0530

    feat: link using pub sub clicks on link are propagated to router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
+import React, { useEffect, useState } from "react";
 import "./styles.css";
 
+function RoutePubSub() {
+  let subscribers = [];
+
+  function subscribe(cb) {
+    subscribers.push(cb);
+  }
+
+  function publish() {
+    subscribers.forEach((cb) => cb());
+  }
+
+  return { subscribe, publish };
+}
+
+const routerPubSub = RoutePubSub();
+
 function Router({ children }) {
-  return <div> {
-    children.find(route => route.props.path === window.location.pathname)
-  } </div>;
+
+  const getCurrentRoute = () => window.location.pathname;
+  const currentPath = getCurrentRoute();
+  const [route, setRoute] = useState(currentPath);
+
+  /**
+   * subscribe to changes and update route 
+   * whenever there's a click on Link 
+   */
+  useEffect(() => {
+    routerPubSub.subscribe(() => setRoute(getCurrentRoute()));
+  }, []);
+
+  return <div>{children.find((child) => child.props.path === route)}</div>;
+}
+
+function Link({ children, to }) {
+  return (
+    <span
+      style={{
+        textDecoration: "underline",
+        cursor: "pointer",
+        padding: "1rem"
+      }}
+      onClick={changeRoute}
+    >
+      {children}
+    </span>
+  );
+
+  function changeRoute() {
+    window.history.pushState({}, "", to);
+    routerPubSub.publish();
+  }
 }
 
 function Home() {
-  return (<div>Home </div>)
+  return <div>Home </div>;
 }
 
 function About() {
-  return (<div>About </div>)
+  return <div>About </div>;
 }
-
 
 export default function App() {
   return (
     <div className="App">
-      <Router> 
+      <nav style={{ textAlign: "left" }}>
+        <Link to="/"> Home </Link>
+        <Link to="/about"> About </Link>
+      </nav>
+      <Router>
         <Home path="/" />
-        <About path="/about"/>
+        <About path="/about" />
       </Router>
     </div>
   );
